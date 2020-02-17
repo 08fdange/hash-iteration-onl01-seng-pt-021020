7a3fb552411319edbee6129545543a34d87a5562
Frank  D'Angelo <08fdange@gmail.com>
2020-02-17 23:47:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_string = ""
   birthday_kids.each do |name, age|
-    
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
 
 
